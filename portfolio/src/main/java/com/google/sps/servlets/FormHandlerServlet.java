f905c8be82dba58afeb61d8268a2a0939c0b5d00
Dylan <dylansmith9001@gmail.com>
2022-06-14 22:37:27 +0000

    Attempt to fix gallery on website

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -5,6 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
@@ -15,11 +20,19 @@ public class FormHandlerServlet extends HttpServlet {
     // Get the value entered in the form.
     String textValue = request.getParameter("text-input");
 
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("Task");
+    FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+            .set("title", textValue)
+            .build();
+    datastore.put(taskEntity);
+
     // Print the value so you can see it in the server logs.
     System.out.println("You submitted: " + textValue);
 
     // Write the value to the response so the user can see it.
     response.getWriter().println("You submitted: " + textValue);
-    response.sendRedirect("https://google.com");
+    response.sendRedirect("/pages/Contact.html");
   }
 }
